2d9810e55a8a0caed06c1dcc71c10d70c63109d6
sanjay7709 <sanjaysundar>
2023-11-16 21:29:45 +0530

    minor

--- PDBC/db1.py
+++ PDBC/db1.py
@@ -18,8 +18,8 @@ To overcome limitations we use (RDBMS)
 
 Module
 -------
-python wants to communicate any database we need to use pre-defined module, such module are not present
-in python library
+python wants to communicate any database we need to use pre-defined module, 
+such module are not present in python library
 so we need to use pre-defined module using "pip"
 >> for oracle database
     mod:  cx_oracle
@@ -34,8 +34,36 @@ Steps:
 =======
 1. import cx_Oracle
 2. connection from python --> DB
+    >> connection from oracle database, we must use ===> connect()
+        syntax: varname= cx_Oracle.connect("connection url")
+                varname is an object of <class,"cx_Oracle.connection">
+        connection url General format is :
+             >>      "Username/password@DNS/serviceid" 
+             >> username==> username of oracle database
+             >> password ==> pass of Oracle database
+             >> DNS==> name of machine where oracle software is installed <ip-address> 
+                       default: 127.0.0.1 or "localhost"
+             >> serviceid ==> On which name DB is avaiable/present in our machine
+                            query: ( select * from global_name; ) 
+        >> if connection url is wrong 
+            we get exception "cx_Oracle.DataBaseError"
 3. create an object of Cursor
+    >> purpose is to carry the query from python to database in the object of cursor 
+    >> We must use ---> cursor() --> present in connection object
+            varname =conobj.cursor()
+            varname is an object of <class,"cx_Oracle.cursor">
 4. Design the query and place the query in object of cursor(DDL,DML,DRL)
+
+    >> 1) DDL --> data definition language
+            a)create
+            b) alter
+            c)drop
+    >> 2) DML --> data manipulation language
+            a) insert
+            b)delete
+            c) update
+    >> 3) DRL -->data retrieval language
+            a) select
 5. python process the result from db
 6. python program closes the connection
 """
